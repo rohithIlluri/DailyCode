99be3146145185804f469d37b121c627d7c1a43c
rohith illuri <56576663+rohithIlluri@users.noreply.github.com>
2023-10-25 11:47:32 -0500

    net-go

--- go/src/go-server/main.go
+++ go/src/go-server/main.go
@@ -1,43 +1,65 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "Post from website! ")
-	name := r.FormValue("name")
-	age := r.FormValue("age")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Age = %s\n", age)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Fprintf(w, "hello!")
-}
+import "fmt"
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Println("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	age := 35
+	name := "Mike"
+
+	//Print
+	fmt.Print("Hello,  ")
+	fmt.Print("World! \n ")
+	fmt.Print("newline \n")
+
+	fmt.Println("hello mike!")
+	fmt.Println("goodbye mike!")
+
+	fmt.Println("my age is ", age, "and my name is ", name)
+
+	//Printf (formatted strings)
+	fmt.Printf("my age is %v and my name is %v \n", age, name)
+	fmt.Printf("my age is %q and my name is %q \n", age, name)
+	fmt.Printf("age is of type %T \n", age)
+	fmt.Printf("you scored %.1f points !\n", 255.22)
+
+	//Sprintf (save formatted strings)
+	var str = fmt.Sprintf("saved_string_format")
+	fmt.Println("the saved string is: ", str)
+
+	//Strings
+
+	var nameOne string = "punk"
+	var nameTwo string = "luigi"
+	fmt.Println(nameOne, nameTwo)
+
+	nameOne = "peach"
+	nameTwo = "bowser"
+	fmt.Println(nameOne, nameTwo)
+
+	nameThree := "batman"
+	fmt.Println(nameThree)
+
+	//ints
+	var ageOne int = 25
+
+	var ageTwo = 30
+
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits & memory
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint16 = 256
+
+	//-128 127
+
+	//float64
+	var scoreOne float64 = 324.34
+	var scoreTWO float64 = 8888888828273738837.7
+	scoreThree := 32
+	fmt.Println(scoreOne, scoreTWO, scoreThree)
+
 }
