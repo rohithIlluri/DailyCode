29698f79801b4895c4d446b85048bf6c7120f666
rohith illuri <56576663+rohithIlluri@users.noreply.github.com>
2023-10-28 10:03:23 -0500

    branch-ref

---
+++ portfolio/src/components/canvas/Rocket.jsx
@@ -0,0 +1,36 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import "./YourStyles.css"; // Include your custom styles here
+
+const Rocket = () => {
+  const rocket = useGLTF("/rocket.gltf"); // Replace with the path to your rocket model.
+
+  return (
+    <group scale={0.02}>
+      <primitive object={rocket.scene} />
+    </group>
+  );
+};
+
+const RocketCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        position: [0, 0, 5],
+      }}
+    >
+      <Suspense fallback={null}>
+        <OrbitControls enableZoom={false} />
+        <Rocket />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default RocketCanvas;
